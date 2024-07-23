a507c9c05521466c02f1373a2a238ba3c85181c0
Alifox20 <gsalifox20@gmail.com>
2024-07-23 11:57:39 +0300

    commit

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+    effect: 'fade',
+    grabCursor: true,
+    
+    navigation: {
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev'
+    }
+});
