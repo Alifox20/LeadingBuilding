dac9f5e71710051105456502a5510edb81eaa6d7
Alifox20 <gsalifox20@gmail.com>
2024-08-01 12:17:51 +0300

    js and other changes

--- app/js/script.js
+++ app/js/script.js
@@ -13,13 +13,6 @@ const swiper = new Swiper('.swiper-container', {
 const tabButtons = document.querySelectorAll('.tabs__link');
 const images = document.querySelectorAll('.tabs__img');
 
-const imageSources = {
-    tab_1: ['../images/building.png', '../images/hotel.png', '../images/house.png', '../images/height.png'],
-    tab_2: ['../images/hotel.png', '../images/building.png', '../images/height.png', '../images/house.png'],
-    tab_3: ['../images/house.png', '../images/height.png', '../images/building.png', '../images/hotel.png'],
-    tab_4: ['../images/height.png', '../images/house.png', '../images/hotel.png', '../images/building.png']
-};
-
 const colors = {
     tab_1: 'blue',
     tab_2: 'red',
@@ -38,17 +31,15 @@ tabButtons.forEach(button => {
         button.classList.add(colors[button.getAttribute('data-tab')]);
 
         const tabId = button.getAttribute('data-tab');
-        const newSources = imageSources[tabId];
 
-        images.forEach((img, index) => {
-            img.src = newSources[index];
+        images.forEach(img => {
+            img.src = img.getAttribute(`data-${tabId}-src`);
         });
-       
     });
 });
 
-images.forEach((img, index) => {
-    img.src = imageSources['tab_1'][index];
+images.forEach(img => {
+    img.src = img.getAttribute('data-tab_1-src');
 });
 
 //Swiper2
